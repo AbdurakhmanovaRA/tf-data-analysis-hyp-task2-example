66e5769a94b3ecdeeb1ecfe30abfeb42487dd89b
Regina <48201923+AbdurakhmanovaRA@users.noreply.github.com>
2023-04-13 22:57:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 241769496 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+        # Применяем критерий Стьюдента для проверки однородности выборок
+    _, pvalue = stats.ttest_ind(x, y)
+    return pvalue >= 0.1 # Ваш ответ, True или False
